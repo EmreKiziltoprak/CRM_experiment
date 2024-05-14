2c7cdade7f2703ef0f1612dd283a2f20f1ac26fe
EmreKiziltoprak <emre.kzltprkk@gmail.com>
2024-05-14 14:40:06 +0300

    database connection is established and current

--- crm-base-api/src/index.ts
+++ crm-base-api/src/index.ts
@@ -1,18 +1,6 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import app from './app';
 
-const app = express();
-const port = process.env.PORT || 3001;
 
-// Middleware
-app.use(bodyParser.json());
-
-// Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3307, () => {
+  console.log(`Server is running on port ${3307}`);
 });
