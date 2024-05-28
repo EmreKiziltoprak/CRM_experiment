529c16d34740cbda7261af8e57128119fd4edae7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-05-28 20:28:13 +0300

    Implement GetInfo with revising controller and service test after implemeting userDetails with user

---
+++ crm-base-api/src/models/userdetails/UserDetails.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from 'typeorm';
+import { Users } from '../users/Users';
+import { IUserDetails } from './IUserDetails';
+
+@Entity({ name: 'user_details' })
+export class UserDetails implements IUserDetails {
+
+  @PrimaryGeneratedColumn({ name: 'detail_id' })
+  detailId!: number;
+
+  @Column({ name: 'first_name', length: 50, type: 'varchar' })
+  firstName!: string;
+
+  @Column({ name: 'last_name', length: 50, type: 'varchar' })
+  lastName!: string;
+
+  @Column({ name: 'language', length: 20, type: 'varchar' })
+  language!: string;
+
+  @Column({ name: 'date_format', length: 20, type: 'varchar' })
+  dateFormat!: string;
+
+  @Column({ name: 'phone_number', length: 20, type: 'varchar' })
+  phoneNumber!: string;
+
+  @Column({ name: 'profile_picture', type: 'text', nullable: true })
+  profilePicture?: string;
+
+  @OneToOne(() => Users, user => user.userDetails)
+  @JoinColumn({ name: 'user_id' })
+  user!: Users;
+
+}
---
+++ crm-base-api/src/models/users/payload/response/UserInfoResponse.ts
@@ -0,0 +1,10 @@
+export interface UserInfoResponse {
+    firstName: string;
+    lastName: string;
+    language: string;
+    dateFormat: string;
+    phoneNumber: string;
+    profilePicture?: string;
+    username?: string;
+    email?: string;
+}
